6e969539aaf86fde10e5c3535bc868305be5c12e
Ice66616 <haha7778595@163.com>
2018-04-17 00:18:23 +0200

    Ice66616

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello,");
+        System.out.println("Hello,world");
+        System.out.println("zhao");
     }
 }
